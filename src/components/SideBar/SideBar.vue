be1044b59eac6626969243f63b6eca240429321e
DobbaWon <charlie.j.dobson@outlook.com>
2025-07-25 21:39:27 +0100

    Set-up the project

---
+++ src/components/SideBar/SideBar.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: 'SideBar'
+}
+</script>
+
+<template>
+  <div class="sidebar">
+    <!-- Sidebar content goes here -->
+  </div>
+</template>
+
+<style scoped>
+.sidebar {
+  width: 20vw;
+  background-color: #000000;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
---
+++ src/components/TextEditor/TextEditor.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: 'TextEditor'
+}
+</script>
+
+<template>
+  <div class="text-editor">
+    <!-- Text editor content goes here -->
+  </div>
+</template>
+
+<style scoped>
+.text-editor {
+  flex: 1;
+  overflow: hidden;
+  background-color: pink;
+}
+</style>
+
